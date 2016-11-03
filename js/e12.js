ebe995ab7b94b8d18aa300bc3f64b2b62b36a1c5
13061121 <13061121@buaa.edu.cn>
2016-11-03 22:18:39 +0800

    优化了11号动态效果并初步添加了12号动态效果

--- js/e12.js
+++ js/e12.js
@@ -1,11 +1,12 @@
-// 11号可视化效果
+// 12号可视化效果
 define(['analyser', 'util'], function (analyser, util) {
+
 
     var cover = 'e11.jpg';
 
     var canvas = util.getById('visual-canvas'),
         ctx = canvas.getContext('2d'),
-        data, i, cx, cy, beginAngle = 0,
+        data, i, cx, cy, angle, beginAngle = 0,
         len = analyser.getFftSize() / 4,
         twoPI = 2 * Math.PI,
         //color = 'rgba(186, 135, 72, 0.5)',
@@ -20,10 +21,11 @@ define(['analyser', 'util'], function (analyser, util) {
         angle = beginAngle;
         var proc = analyser.getCurrentTime() / analyser.getDuration();
         cx = canvas.width / 2;
-        cy = canvas.height / 2;
+        cy = canvas.height * 0.45;
         var startx = canvas.width * 0.1;
         var starty = canvas.height * 0.9;
         var processbarWdith = canvas.width * 0.8;
+        var r = Math.min(canvas.width / 4, canvas.height / 4);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.globalCompositeOperation = 'lighter';
         var total = 0;
@@ -61,15 +63,15 @@ define(['analyser', 'util'], function (analyser, util) {
         ctx.strokeStyle = "white";
         ctx.lineWidth = 3;
         ctx.beginPath();
-        ctx.arc(startx + proc * processbarWdith, starty, 18, rotateAngle, rotateAngle+Math.PI/4);
-        ctx.arc(startx + proc * processbarWdith, starty, 18, rotateAngle+Math.PI/4,rotateAngle,true);
+        ctx.arc(startx + proc * processbarWdith, starty, 18, rotateAngle, rotateAngle + Math.PI / 4);
+        ctx.arc(startx + proc * processbarWdith, starty, 18, rotateAngle + Math.PI / 4, rotateAngle, true);
         ctx.closePath();
         ctx.stroke();
 
 
         ctx.beginPath();
-        ctx.arc(startx + proc * processbarWdith, starty, 18, rotateAngle+Math.PI, rotateAngle+5*Math.PI/4);
-        ctx.arc(startx + proc * processbarWdith, starty, 18, rotateAngle+5*Math.PI/4,rotateAngle+Math.PI, true);
+        ctx.arc(startx + proc * processbarWdith, starty, 18, rotateAngle + Math.PI, rotateAngle + 5 * Math.PI / 4);
+        ctx.arc(startx + proc * processbarWdith, starty, 18, rotateAngle + 5 * Math.PI / 4, rotateAngle + Math.PI, true);
         ctx.closePath();
         ctx.stroke();
 
@@ -80,35 +82,58 @@ define(['analyser', 'util'], function (analyser, util) {
         ctx.closePath();
         ctx.fill();
 
+        //混淆music数据
+
+        var outR = new Array(),
+            inR = new Array(),
+            ang, x, y, musicData,
+            seg = twoPI / len;
+
+        for (i = 0; i < len; i++) {
+            var t = (i * 7) % len;
+            ang = angle + t * seg;
+            musicData = ( data[(i + len - 2) % len] + data[(i + len - 1) % len] + data[i] + data[(i + 1) % len] + data[(i + 2) % len] ) / 10;
+            var cs = Math.cos(ang), sn = Math.sin(ang);
+            x = (r + musicData) * cs;
+            y = (r + musicData) * sn;
+            outR[t] = [ang, x, y];
+            x = (r - musicData) * cs;
+            y = (r - musicData) * sn;
+            inR[t] = [ang, x, y];
+        }
+
+
         // 绘制music visualizer
-        var wid = canvas.width*0.8/len;
-        var heig = canvas.height / 2;
-        var color_t1 ='rgba(220, 0, 0, 0.9)';
-        var color_t2 ='rgba(0, 225, 0, 0.9)';
-        var grd=ctx.createLinearGradient(0,canvas.height*0.25,0,canvas.height*0.75);
-        grd.addColorStop(0,color_t1);
-        grd.addColorStop(.5,color_t2);
-        grd.addColorStop(1,color_t1);
+        var color_t1 = 'rgba(220, 0, 0, 0.9)';
+        var color_t2 = 'rgba(20, 225, 25, 0.9)';
+        var grd = ctx.createLinearGradient(0, canvas.height * 0.25, 0, canvas.height * 0.75);
+        grd.addColorStop(0, color_t1);
+        grd.addColorStop(.5, color_t2);
+        grd.addColorStop(1, color_t1);
         //ctx.strokeStyle = color;
         ctx.strokeStyle = grd;
         ctx.lineWidth = 2;
-        for (i = 0; i < len; i += 1) {
-            var t = canvas.width*0.1+i*wid;
+        var t = len - 1;
+        for (i = 0; i < len; i ++) {
             ctx.beginPath();
-            ctx.moveTo(t,  heig-data[i]);
-            ctx.lineTo(t,  heig+data[i]);
-            ctx.lineTo(t+wid,  heig+data[i]);
-            ctx.lineTo(t+wid,  heig-data[i]);
+            ctx.moveTo(cx + outR[t][1], cy + outR[t][2]);
+            ctx.lineTo(cx + outR[i][1], cy + outR[i][2]);
+            ctx.lineTo(cx + inR[i][1], cy + inR[i][2]);
+            ctx.lineTo(cx + inR[t][1], cy + inR[t][2]);
+            ctx.lineTo(cx + outR[t][1], cy + outR[t][2]);
+            //ctx.arc(cx + (inR[i][1]+outR[i][1])/2, cy + (inR[i][2]+outR[i][2])/2, 2, angle, angle + twoPI);
             ctx.closePath();
             ctx.stroke();
+            console.log(outR[i][1] - outR[t][1]);
+            t = i;
         }
-        beginAngle = (beginAngle + 0.00001 * total) % twoPI;
+        beginAngle = (beginAngle + 0.005) % twoPI; //旋转速度
 
         ctx.restore();
     }
 
     function init() {
-        util.setBg(11);
+        util.setBg(12);
         initOrNot = true;
     }
 
